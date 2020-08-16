2b297431d76a4e3653c3f4b7f354ea742df97784
Aidan Goettsch <aidangoettsch@gmail.com>
2020-08-16 17:03:52 -0400

    actually have the app.py

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+import numpy as np
+import pandas as pd
+from gspread_pandas import Spread, Client
+from sqlalchemy import create_engine
+import pymysql
+import tbaapiv3client
+import dotenv
+import os
+from flask import Flask, request
+
+dotenv.load_dotenv()
 app = Flask(__name__)
+sql_engine = create_engine('mysql+pymysql://root:reee@127.0.0.1/scouting', pool_recycle=3600)
+db_connection = sql_engine.connect()
+
+
+EVENT = os.getenv("EVENT")
+TBA_API_KEY = os.getenv("TBA_API_KEY")
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/load_data', methods=['POST'])
+def load_data():
+    spread = Spread(request.json["document"])
+    df = spread.sheet_to_df(sheet=spread.sheets[0])
+    df.index = pd.to_datetime(df.index)
+    # del df['Timestamp']
+    df.to_sql("data", db_connection, if_exists="replace", index=True, index_label='id')
+    return 'success!'
 
 
 if __name__ == "__main__":
