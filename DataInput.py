ed76d3e5067863de82741d6047eb44128e6926ec
samch <samchoks@hotmail.com>
2020-09-16 10:00:00 -0400

    Now configures MySQL automatically from TBA

--- DataInput.py
+++ DataInput.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine, Table, exists
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, BigInteger, ForeignKey, Integer, String, Boolean
+from sqlalchemy import Column, BigInteger, ForeignKey, Integer, String, Boolean, Float
 from sqlalchemy.orm import relationship, sessionmaker
 import json
+import numpy
+import requests
+import pandas as pd
 
 Base = declarative_base()
 
@@ -58,11 +61,15 @@ class DataInput:
 
         self.TeamDataObject = TeamData
         self.MatchDataObject = MatchData
+
+        self.tbaLastModified = 'Wed, 1 Jan 100 00:00:01 GMT'
+
+        self.config = {}
         self.parseConfig()
+        self.parseSQLConfig()
 
         Base.metadata.create_all(self.engine)
-        t1 = self.TeamDataObject(teamid=40999,match="q46",event="vahay",PowerCells=40)
-        self.session.add(t1)
+
         self.session.commit()
 
     def addMatch(self, id: int, red_teams_num: list, blue_teams_num: list):
@@ -82,17 +89,21 @@ class DataInput:
         self.session.add(m)
         self.session.commit()
 
-    def addTeam(self, id):
+    def getTeam(self, id):
         if self.checkIfExists(Teams, id):
-            raise Exception('Team already exists')
-
-        t = Teams(id=id)
-        self.session.add(t)
-        self.session.commit()
-
-    def getTeam(self,id):
-        if self.checkIfExists(Teams,id):
             return self.session.query(Teams).filter_by(id=id)[0]
+        else:
+            raise Exception('Team does not exist')
+
+    def getTeamData(self, id):
+        if self.checkIfExists(self.TeamDataObject, id):
+            return self.session.query(self.TeamDataObject).filter_by(id=id)[0]
+        else:
+            raise Exception('Team Data does not exist')
+
+    def getMatch(self, id):
+        if self.checkIfExists(Matches, id):
+            return self.session.query(Matches).filter_by(id=id)[0]
         else:
             raise Exception('Team does not exist')
 
@@ -100,38 +111,83 @@ class DataInput:
         (ret,), = self.session.query(exists().where(obj.id == id))
         return ret
 
-    def parseConfig(self):
+    def parseSQLConfig(self):
         try:
 
-            with open('./SQLconfig.json') as f:
-                config = json.load(f)
+            with open('config/SQLconfig.json') as f:
+                SQLconfig = json.load(f)
 
-            t_data = {"__tablename__": f'TeamData{config["TeamDataConfig"]["Year"]}',
+            t_data = {"__tablename__": f'TeamData{SQLconfig["TeamDataConfig"]["Year"]}',
+                      "__table_args__": {'extend_existing': True},
                       "id": Column(Integer, primary_key=True),
                       "teamid": Column(Integer, ForeignKey('team.id')),
                       "match": Column(String(50)),
                       "event": Column(String(50))}
 
-            config['TeamDataConfig']['Attributes'] = {k: eval(v) for k, v in
-                                                      config['TeamDataConfig']['Attributes'].items()}
-            self.TeamDataObject = type(f'TeamData{config["TeamDataConfig"]["Year"]}', (Base,),
-                                       {**config['TeamDataConfig']['Attributes'], **t_data})
-            Teams.data_list = relationship(f'TeamData{config["TeamDataConfig"]["Year"]}')
+            SQLconfig['TeamDataConfig']['Attributes'] = {k: eval(v) for k, v in
+                                                         SQLconfig['TeamDataConfig']['Attributes'].items()}
+            self.TeamDataObject = type(f'TeamData{SQLconfig["TeamDataConfig"]["Year"]}', (Base,),
+                                       {**SQLconfig['TeamDataConfig']['Attributes'], **t_data})
+            Teams.data_list = relationship(f'TeamData{SQLconfig["TeamDataConfig"]["Year"]}')
 
-            m_data = {"__tablename__": f'MatchData{config["MatchDataConfig"]["Year"]}',
+            m_data = {"__tablename__": f'MatchData{SQLconfig["MatchDataConfig"]["Year"]}',
+                      "__table_args__": {'extend_existing': True},
                       "id": Column(Integer, primary_key=True),
                       "matchId": Column(Integer, ForeignKey('match.id')),
                       "winner": Column(Boolean)}
 
-            config['MatchDataConfig']['Attributes'] = {k: eval(v) for k, v in
-                                                       config['MatchDataConfig']['Attributes'].items()}
-            self.MatchDataObject = type(f'MatchData{config["MatchDataConfig"]["Year"]}', (Base,),
-                                        {**config['MatchDataConfig']['Attributes'], **m_data})
-            Matches.data_list = relationship(f'MatchData{config["MatchDataConfig"]["Year"]}')
+            SQLconfig['MatchDataConfig']['Attributes'] = {k: eval(v) for k, v in
+                                                          SQLconfig['MatchDataConfig']['Attributes'].items()}
+            self.MatchDataObject = type(f'MatchData{SQLconfig["MatchDataConfig"]["Year"]}', (Base,),
+                                        {**SQLconfig['MatchDataConfig']['Attributes'], **m_data})
+            Matches.data_list = relationship(f'MatchData{SQLconfig["MatchDataConfig"]["Year"]}')
 
         except FileNotFoundError:
             pass
 
+    def getTBAData(self, event: str = ''):
+        headers = {'X-TBA-Auth-Key': self.config['TBA-Key'], 'If-Modified-Since': self.tbaLastModified}
+        r = requests.get(f'https://www.thebluealliance.com/api/v3/event/{event}/matches', headers=headers)
+        self.tbaLastModified = r.headers['Last-Modified']
+        data = pd.json_normalize(r.json())
+        data.drop(['videos', 'score_breakdown'], axis=1)
+        data = data.infer_objects()
+
+        # TODO: Finish Method with getMatch function
+
+        return r
+
+    def parseConfig(self):
+        with open('config/config.json') as f:
+            config = json.load(f)
+
+        self.config = config
+
+        headers = {'X-TBA-Auth-Key': self.config['TBA-Key'], 'If-Modified-Since': self.tbaLastModified}
+        r = requests.get(f'https://www.thebluealliance.com/api/v3/event/{self.config["Year"]}vahay/matches',
+                         headers=headers)
+        data = pd.json_normalize(r.json())
+        drop_list = ['videos','score_breakdown']
+        for d in drop_list:
+            try:
+                data.drop(d, axis=1)
+            except KeyError:
+                pass
+        data = data.infer_objects()
+        matchDataConfig = {}
+        for col, dtype in zip(data.columns, data.dtypes):
+            if dtype == numpy.float64:
+                matchDataConfig[col] = f'Column(Float)'
+            elif dtype == numpy.int64:
+                matchDataConfig[col] = f'Column(Integer)'
+            elif dtype == numpy.object:
+                matchDataConfig[col] = f'Column(String(100))'
+
+        originalConfig = json.load(open('./config/SQLconfig.json', 'r'))
+        with open('./config/SQLconfig.json', 'w') as f:
+            originalConfig['MatchDataConfig']['Attributes'] = matchDataConfig
+            json.dump(originalConfig, f, indent=4)
+
 
 d = DataInput()
-print(d.getTeam(40999).data_list)
+
